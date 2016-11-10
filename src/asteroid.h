2f0c61eb626f1cad524083f4deda826d1162eb4c
Jonthan Oakes <darth62969@gmail.com>
2016-11-10 13:25:34 -0500

    now shit is good

--- src/asteroid.h
+++ src/asteroid.h
@@ -1,41 +1,28 @@
-<<<<<<< HEAD
-=======
+
 #ifndef __ASTEROID_H_INCLUDED__
 #define __ASTEROID_H_INCLUDED__
 
->>>>>>> 6f3f8e3cec2b25a9b0eaa7faac6c3067d7588bd2
+
 #include "headers.h"
 #include "structs.h"
 
 class asteroid
 {
 	public:
-        	asteroid createAsteroid();
+        asteroid createAsteroid();
 		asteroid breakupAsteroid(asteroid a);
 
-<<<<<<< HEAD
+
 	private:
 		//triangle * triptr;
 		//triangle * nexttriptr;
 		// point origin;
-        	int numsides;
-		int max_x;
-	    	int max_y;
-        	//asteroid createAsteroid(triangle a);
-	    	asteroid tessilateAsteriod();	
-=======
-    private:
-        float rotation;
-        point translation;
-        point origin;
-        std::vector<point> astPnts;
-        std::vector<triangle> astTris;
-        bool clipped;
         int numsides;
-	    int max_x;
+		int max_x;
 	    int max_y;
         //asteroid createAsteroid(triangle a);
-	    asteroid tessilateAsteriod();
-	
->>>>>>> 6f3f8e3cec2b25a9b0eaa7faac6c3067d7588bd2
+	    asteroid tessilateAsteriod();	
+
+
+
 };
--- src/structs.h
+++ src/structs.h
@@ -17,13 +17,8 @@ struct asteriod
 {
     float rotation;
     point translation;
-<<<<<<< HEAD
-    vector<point> astPnts;
-    vector<triangle> astTris;
-=======
     std::vector<point> astPnts;
     std::vector<triangle> astTris;
->>>>>>> 6f3f8e3cec2b25a9b0eaa7faac6c3067d7588bd2
     bool clipped;
 };
 
