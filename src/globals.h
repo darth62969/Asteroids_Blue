3326515f5bdfefbe353f56797ba8a4fe5a3cd038
Jonathan Oakes <darth62969@gmail.com>
2016-11-11 12:37:46 -0500

    we forgot tot add prototypes and globals to the git.

---
+++ src/globals.h
@@ -0,0 +1,23 @@
+#ifndef GLOBALS
+#define GLOBALS
+
+#include "structs.h"
+#include "headers.h"
+
+extern ship enterprise;
+/*
+extern const int WINDOW_MAX_X;
+extern const int WINDOW_MAX_Y;
+extern const int WINDOW_POSITION_X;
+extern const int WINDOW_POSITION_Y;
+extern const int WORLD_COORDINATE_MIN_X;
+extern const int WORLD_COORDINATE_MIN_Y;
+extern const int WORLD_COORDINATE_MAX_X;
+extern const int WORLD_COORDINATE_MAX_Y;
+
+//Asteroid Contants
+extern const int ASTEROID_MAX_X;
+extern const int ASTEROID_MAX_Y;
+*/
+#endif
+
---
+++ src/prototypes.h
@@ -0,0 +1,8 @@
+#ifndef PROTOTYPES
+#define PROTOTYPES
+
+#include "structs.h"
+
+ship createShip(void);
+
+#endif
